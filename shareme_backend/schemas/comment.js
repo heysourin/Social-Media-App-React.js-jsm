33c6e2e4442e54c98c816d3cfa50cdb5325ce47f
heysourin <sourin07@gmail.com>
2022-11-22 14:39:12 +0530

    Backend created usning 'Sanity.io'

---
+++ shareme_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment1",
+  title: "Comment1",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "PostedBy",
+      type: "postedBy",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
