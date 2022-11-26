f02b9af6630e78d5ce442b4dd356fc8758f8fe96
heysourin <sourin07@gmail.com>
2022-11-26 10:56:55 +0530

    Updated

---
+++ shareme_frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      margin: {
+        320:'320',
+      },
+      width: {
+        190:'190',
+      },
+    },
+  },
+  plugins: [],
+}
